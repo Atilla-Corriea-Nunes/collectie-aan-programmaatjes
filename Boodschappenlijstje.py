c8a18fdef6e22f6e2c4fdf929dcb5f45acd2de5c
Atilla <you@example.com>
2021-09-28 12:08:14 +0200

    finished the grocery list, its lookin quite good

---
+++ Boodschappenlijstje.py
@@ -0,0 +1,27 @@
+import random
+shoppinglist = {}
+x = 0
+
+def addToShoppinglist(currentitem):
+        if x == 1:
+            pass
+        elif currentitem in shoppinglist:
+            shoppinglist[currentitem] += 1
+        else:
+            shoppinglist.update({currentitem : 1})
+        
+        return shoppinglist
+
+
+
+while True:
+    item = input("Welk item wilt u aan het boodschappenlijstje toevoegen? ")
+    addToShoppinglist(item)    
+    leave = input("Wilt u nog een item toevoegen? (Y/N) ")
+    if (leave.lower() == "y"):
+        continue
+    else:
+        x = 1
+        print(addToShoppinglist(item))
+        break
+        
