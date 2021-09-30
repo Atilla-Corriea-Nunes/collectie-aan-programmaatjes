4addd1e5ce1344243ac7daedae26adac6f03a362
Atilla <you@example.com>
2021-09-30 13:04:52 +0200

    want a good password? just use passwordgenerator.py and get epic randomized results!

---
+++ Passwordgenerator.py
@@ -0,0 +1,30 @@
+import random
+import string
+themissingones = []
+password = []
+letterslow = string.ascii_lowercase
+lettersup = string.ascii_uppercase
+digits = string.digits
+specialchar = ["@", "#", "$", "%", "&", "_", "?"]
+upamount = (random.randint(2, 6))
+numamount = (random.randint(4, 7))
+for x in range(upamount):
+    password.append(random.choice(lettersup))
+for x in range (0,8):
+    password.append(random.choice(letterslow))
+first = password[:3]
+last = password[len(password)-1]
+middleoflist = list(password[3: len(password)-1:])
+for x in range (0,3):
+    middleoflist.append(random.choice(specialchar))
+for x in range (numamount):
+    middleoflist.append(random.choice(digits))    
+themissingones = list(first) + list(last)
+
+while (len(middleoflist) < 20):
+    middleoflist.append(random.choice(letterslow))
+random.shuffle(middleoflist)
+random.shuffle(themissingones)
+first = themissingones[:3]
+finishedPassword = list(first) + list(middleoflist) + list(last)
+print(finishedPassword)
